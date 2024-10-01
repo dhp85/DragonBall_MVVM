cdf8e818d2d453e76421a6f08f0adf6a74eed5c3
Diego Herreros <diegohp85@gmail.com>
2024-10-01 11:36:48 +0200

    Initial Commit

---
+++ DragonBall_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DragonBall_MVVM
+//
+//  Created by Diego Herreros Parron on 1/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DragonBall_MVVMUITests/DragonBall_MVVMUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  DragonBall_MVVMUITestsLaunchTests.swift
+//  DragonBall_MVVMUITests
+//
+//  Created by Diego Herreros Parron on 1/10/24.
+//
+
+import XCTest
+
+final class DragonBall_MVVMUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
